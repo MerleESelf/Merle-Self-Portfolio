f441fed93cfdbe4720cf5be9855cb5c1d09a704f
MerleESelf <merleself@gmail.com>
2023-01-26 13:39:32 -0500

    pleasseeeee

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,5 @@ import vercel from '@astrojs/vercel/static';
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
-  adapter: vercel({
-    includeFiles: ['./public/assets/Merle Self Resume.pdf']
-  }),
+  adapter: vercel(),
 });
